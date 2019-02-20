dd85edf984fc2fed809d06d2900a96c11ede28eb
Lindsay Terry <lterry.development@gmail.com>
2019-02-19 23:10:02 -0800

    updated jawsdb

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,9 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection ({
     host: "localhost",
+    port: 3306;
     user: "root",
-    password: "password",
+    password: "",
     database: "burgers_db"
   });
 };
